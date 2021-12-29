576134fdf09c548c33f89a9d385ad63db3d56494
Himanshu Patil <86556273+HIMANSHU22-16@users.noreply.github.com>
2021-12-29 15:29:08 +0530

    Add files via upload from device

---
+++ game.js
@@ -0,0 +1,63 @@
+'use strict';
+
+alert("----------------------------------------------\n\n\n In this Game we give you 5 chances to find the secrate number between 1 to 20. Use your brain well and find the number. \n\n Good Luck :) \n\n")
+
+
+let secretNumber = Math.trunc(Math.random()*20)+1;
+let score = 5;
+let highscore = 0;
+
+console.log(secretNumber);
+
+function displayMessage(msg){
+  document.querySelector('.message').textContent = msg;
+}
+document.querySelector('.again').addEventListener('click', function() {
+  score = 5;
+  document.querySelector('.score').textContent=5;
+  document.querySelector('.highscore').textContent=0;
+  document.querySelector('body').style.backgroundColor = '#000';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+    displayMessage('Start playing');
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = document.querySelector('.guess').value;
+
+  if(guess < 1 || guess > 20)
+  {
+    displayMessage('Invalid Input');
+  }
+  else if(guess == secretNumber) 
+  {
+    displayMessage('Correct Number');
+    document.querySelector('.number').textContent = secretNumber;
+    document.body.style.backgroundColor = '#34CC17';
+
+      if(score > highscore) 
+      {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+  } 
+  else if (guess !== secretNumber)
+  {
+      if(score > 1)
+      {
+       displayMessage(guess > secretNumber ? 'Too high!' : 'Too Low!');
+       score--;
+       document.querySelector('.score').textContent = score;
+      }
+      else 
+      {
+       displayMessage('You lost the game!');
+       document.querySelector('.score').textContent=0;
+      }
+  }
+});
+
+
+
+
+
